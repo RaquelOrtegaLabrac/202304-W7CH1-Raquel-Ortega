32764b144a18820e12606b51149eeb083eb47077
labrac <raquelortegalabrac@hotmail.com>
2023-06-12 23:27:01 +0200

    Update router.ts

--- src/router.ts
+++ src/router.ts
@@ -2,8 +2,10 @@ import { Router as createRouter } from 'express';
 import { DataController } from './controller.js';
 import { Subject } from './entities/sample.js';
 import { Repo } from './repository/repo.js';
+import { DataRepo } from './repository/repository.js';
 
-const controller = new DataController();
+const repo: Repo<Subject> = new DataRepo();
+const controller = new DataController(repo);
 export const dataRouter = createRouter();
 
 dataRouter.get('/', controller.getAll.bind(controller));
